0560749580c7a574feff4449741ba4065f7c9910
涂子彧 11372011 <bryantu0111@gmail.com>
2025-03-06 20:54:12 +0800

    Update Problem_2.py

--- HW1/Problem_2.py
+++ HW1/Problem_2.py
@@ -7,5 +7,6 @@ x = float(input('請輸入一個公比:'))
 m_table = [3, 10, 50, 200, 400]
 
 m_lst = [1/(1-x)]
-for m in m_table + m_lst:
+for m in m_table :
     print(f'公比是{x}時，總和是{series(x,m)} ')
+print(m_lst)
